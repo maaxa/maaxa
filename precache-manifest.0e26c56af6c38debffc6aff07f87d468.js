1c137eeadd619e04cf6400ad75facfc7f677a283
test test <test@yandex.ru>
2019-11-30 21:33:49 +0300

    Updates

---
+++ precache-manifest.0e26c56af6c38debffc6aff07f87d468.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ead5800b24a0af9f1825ae772363963f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d2a6124ca09a1ba99e0",
+    "url": "/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "a2965f4a8f50f00ad865",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "6d2a6124ca09a1ba99e0",
+    "url": "/static/js/2.d1b912c0.chunk.js"
+  },
+  {
+    "revision": "a2965f4a8f50f00ad865",
+    "url": "/static/js/main.9de85f20.chunk.js"
+  },
+  {
+    "revision": "4e59c44544c186a57446",
+    "url": "/static/js/runtime-main.24aa0ad3.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
