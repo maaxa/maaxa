d605b9ffb8e7911b903781be912e9fd9c6ecb4c3
test test <test@yandex.ru>
2019-12-01 19:07:32 +0300

    1**

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import connect from '@vkontakte/vk-connect';
 import View from '@vkontakte/vkui/dist/components/View/View';
-import ScreenSpinner from '@vkontakte/vkui/dist/components/ScreenSpinner/ScreenSpinner';
+//import ScreenSpinner from '@vkontakte/vkui/dist/components/ScreenSpinner/ScreenSpinner';
 import '@vkontakte/vkui/dist/vkui.css';
 
 import Home from './panels/Home';
@@ -10,7 +10,7 @@ import Persik from './panels/Persik';
 const App = () => {
 	const [activePanel, setActivePanel] = useState('home');
 	const [fetchedUser, setUser] = useState(null);
-	const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
+	//const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
 
 	useEffect(() => {
 		connect.subscribe(({ detail: { type, data }}) => {
@@ -36,7 +36,7 @@ const App = () => {
 			const user = await connect.sendPromise('VKWebAppGetUserInfo');
 			//const userr = await connect.sendPromise('VKWebAppGetPhoneNumber');
 			setUser(user);
-			setPopout(null);
+			//setPopout(null);
 		}
 		fetchData();
 	}, []);
@@ -48,8 +48,15 @@ const App = () => {
 		setActivePanel(e.currentTarget.dataset.to);
 	};
 
-	return (
+	/*return (
 		<View activePanel={activePanel} popout={popout}>
+			<Home id='home' fetchedUser={fetchedUser} go={go} />
+			<Persik id='persik' go={go} />
+		</View>
+	);*/
+	
+	return (
+		<View activePanel={activePanel} >
 			<Home id='home' fetchedUser={fetchedUser} go={go} />
 			<Persik id='persik' go={go} />
 		</View>
--- src/panels/Home.js
+++ src/panels/Home.js
@@ -1,16 +1,61 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
-import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
-import Button from '@vkontakte/vkui/dist/components/Button/Button';
-import Group from '@vkontakte/vkui/dist/components/Group/Group';
-import Cell from '@vkontakte/vkui/dist/components/Cell/Cell';
-import Div from '@vkontakte/vkui/dist/components/Div/Div';
-import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
+import { Panel, PanelHeader, Button, Group, Div,HorizontalScroll,Avatar,Cell} from '@vkontakte/vkui'
+//import PropTypes from 'prop-types';
+//import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
+//import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
+//import Button from '@vkontakte/vkui/dist/components/Button/Button';
+//import Group from '@vkontakte/vkui/dist/components/Group/Group';
+//import Cell from '@vkontakte/vkui/dist/components/Cell/Cell';
+//import Div from '@vkontakte/vkui/dist/components/Div/Div';
+//import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
+	//import Icon24Back from '@vkontakte/icons/dist/24/back';
+//import HeaderButton from '@vkontakte/vkui/dist/components/HeaderButton/HeaderButton';
+	//import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
+	//import Icon24Search from '@vkontakte/icons/dist/24/search'
+
+
+//const osname = platform();
+
 
 const Home = ({ id, go, fetchedUser }) => (
+
+
+
+
 	<Panel id={id}>
 		<PanelHeader>Мое приложение</PanelHeader>
+		
+		<Group style={{ paddingBottom: 8 }}>
+        <HorizontalScroll>
+          <div style={{ display: 'flex' }}>
+            <div >
+               <Button level="outline">пицца d30 см</Button>
+            </div>
+            <div >
+              <Button level="outline">пицца d40 см</Button>
+            </div>
+            <div >
+               <Button level="outline">подарочные карты</Button>
+            </div>
+            <div >
+               <Button level="outline">пироги до 1600</Button>
+            </div>
+            <div >
+               <Button level="outline">суши и роллы</Button>
+            </div>
+            <div >
+              <Button level="outline">бургеры</Button>
+            </div>
+            <div >
+              <Button level="outline">горячие блюда</Button>
+            </div>
+            <div >
+              Вадим
+            </div>
+          </div>
+        </HorizontalScroll>
+      </Group>
+		
 		{fetchedUser &&
 		<Group title="пользователь вк">
 			<Cell
@@ -23,7 +68,7 @@ const Home = ({ id, go, fetchedUser }) => (
 				
 			</Cell>
 		</Group>}
-
+			
 		<Group title="Navigation Example">
 			<Div>
 				<Button size="xl" level="2" onClick={go} data-to="persik">
@@ -31,10 +76,15 @@ const Home = ({ id, go, fetchedUser }) => (
 				</Button>
 			</Div>
 		</Group>
+		
+		
+		
 	</Panel>
+	
+	
 );
 
-Home.propTypes = {
+/*Home.propTypes = {
 	id: PropTypes.string.isRequired,
 	go: PropTypes.func.isRequired,
 	fetchedUser: PropTypes.shape({
@@ -47,6 +97,10 @@ Home.propTypes = {
 			title: PropTypes.string,
 		}),
 	}),
-};
+};*/
+
+
+
+
 
 export default Home;
